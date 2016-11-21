aed670454f257b7394d8af20e0125871c93b0314
MkFair <magisterK@yandex.ru>
2016-11-22 00:00:54 +0300

    Signed-off-by: MkFair <magisterK@yandex.ru>

---
+++ view/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title><?=$title?></title>
+<link rel='stylesheet' src='css/main.css'/>
+<script src="js/main.js"></script>
+</head>
+
+<body>
+
+
+
+
+
+
+
+
+</body>
+</html>
